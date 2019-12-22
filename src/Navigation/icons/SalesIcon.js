48fd6458b5c7db14843c43ce44518cec4b233824
Arisha <ar.kuzmina24@gmail.com>
2019-12-22 21:53:55 +0300

    SVG refactoring

--- src/Navigation/icons/SalesIcon.js
+++ src/Navigation/icons/SalesIcon.js
@@ -3,20 +3,28 @@ import React from 'react';
 class SalesIcon extends React.Component {
     render() {
         return (
-            <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 20 20" xmlSpace="preserve">
-            <rect x="1" y="4" width="1" height="15"/>
-            <rect x="1" y="18" width="18" height="1"/>
-            <g>
-                <path d="M6,12v3H5v-3H6 M7,11H4v5h3V11L7,11z"/>
-            </g>
-            <g>
-                <path d="M11,10v5h-1v-5H11 M12,9H9v7h3V9L12,9z"/>
-            </g>
-            <g>
-                <path d="M16,8v7h-1V8H16 M17,7h-3v9h3V7L17,7z"/>
-            </g>
-            <polyline class="st0" points="4,9 10,4 12,6 17,1 "/>
+            <svg 
+                version="1.1" 
+                id="Слой_1" 
+                xmlns="http://www.w3.org/2000/svg" 
+                xmlnsXlink="http://www.w3.org/1999/xlink" 
+                x="0px" 
+                y="0px"
+                viewBox="0 0 20 20" 
+                xmlSpace="preserve"
+            >
+                <rect x="1" y="4" width="1" height="15"/>
+                <rect x="1" y="18" width="18" height="1"/>
+                <g>
+                    <path d="M6,12v3H5v-3H6 M7,11H4v5h3V11L7,11z"/>
+                </g>
+                <g>
+                    <path d="M11,10v5h-1v-5H11 M12,9H9v7h3V9L12,9z"/>
+                </g>
+                <g>
+                    <path d="M16,8v7h-1V8H16 M17,7h-3v9h3V7L17,7z"/>
+                </g>
+                <polyline class="st0" points="4,9 10,4 12,6 17,1 "/>
             </svg>
         );
     }
--- src/Navigation/icons/WorkingDaysIcon.js
+++ src/Navigation/icons/WorkingDaysIcon.js
@@ -3,25 +3,33 @@ import React from 'react';
 class WorkingDaysIcon extends React.Component {
     render() {
         return (
-            <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 20 20" xmlSpace="preserve">
-            <g>
-                <rect x="3.0036621" y="8.4868164" width="2" height="2"/>
-                <rect x="7" y="8.5" width="2" height="2"/>
-                <rect x="3" y="11.5" width="2" height="2"/>
-                <rect x="7" y="11.5" width="2" height="2"/>
-                <rect x="3.0036621" y="14.5048828" width="2" height="2"/>
-                <rect x="7" y="14.5" width="2" height="2"/>
-                <rect x="11" y="8.5" width="2" height="2"/>
-                <rect x="11" y="11.5" width="2" height="2"/>
-                <rect x="11" y="14.5" width="2" height="2"/>
-                <rect x="15" y="8.5" width="2" height="2"/>
-                <rect x="15" y="11.5" width="2" height="2"/>
-                <rect x="15" y="14.5" width="2" height="2"/>
-                <path d="M18,2.5h-2v-1h-1v1H5v-1H4v1H2c-0.5499878,0-1,0.4500122-1,1v3v1v10c0,0.5499878,0.4500122,1,1,1h16
-                    c0.5499878,0,1-0.4500122,1-1v-10v-1v-3C19,2.9500122,18.5499878,2.5,18,2.5z M18,17.5H2v-10h16V17.5z M2,6.5v-3h2v1h1v-1h10v1h1
-                    v-1h2v3H2z"/>
-            </g>
+            <svg 
+                version="1.1" 
+                id="Слой_1" 
+                xmlns="http://www.w3.org/2000/svg" 
+                xmlnsXlink="http://www.w3.org/1999/xlink" 
+                x="0px" 
+                y="0px"
+                viewBox="0 0 20 20" 
+                xmlSpace="preserve"
+            >
+                <g>
+                    <rect x="3.0036621" y="8.4868164" width="2" height="2"/>
+                    <rect x="7" y="8.5" width="2" height="2"/>
+                    <rect x="3" y="11.5" width="2" height="2"/>
+                    <rect x="7" y="11.5" width="2" height="2"/>
+                    <rect x="3.0036621" y="14.5048828" width="2" height="2"/>
+                    <rect x="7" y="14.5" width="2" height="2"/>
+                    <rect x="11" y="8.5" width="2" height="2"/>
+                    <rect x="11" y="11.5" width="2" height="2"/>
+                    <rect x="11" y="14.5" width="2" height="2"/>
+                    <rect x="15" y="8.5" width="2" height="2"/>
+                    <rect x="15" y="11.5" width="2" height="2"/>
+                    <rect x="15" y="14.5" width="2" height="2"/>
+                    <path d="M18,2.5h-2v-1h-1v1H5v-1H4v1H2c-0.5499878,0-1,0.4500122-1,1v3v1v10c0,0.5499878,0.4500122,1,1,1h16
+                        c0.5499878,0,1-0.4500122,1-1v-10v-1v-3C19,2.9500122,18.5499878,2.5,18,2.5z M18,17.5H2v-10h16V17.5z M2,6.5v-3h2v1h1v-1h10v1h1
+                        v-1h2v3H2z"/>
+                </g>
             </svg>
         );
     }
